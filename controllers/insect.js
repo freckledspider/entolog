3bd9ddbc5677f667eaf4a16d2516043aafdd4114
bri <bcamesi@gmail.com>
2022-12-09 13:21:50 -0500

    Working on show page

--- controllers/insect.js
+++ controllers/insect.js
@@ -8,10 +8,24 @@ const router = express.Router()
 
 
 // routes
+
+// on /insects, list all insects in database
 router.get('/', (req, res) => {
+    // find the insects on mongoDB
     Insect.find({})
+    // then render on page
     .then((insects) => {
-        res.render('insects/index.ejs', { insects })
+        res.render('insects/index.ejs', {insects})
+    })
+})
+
+// on insects/show.ejs, give more details for specific insect in database
+router.get('/:id', async (req, res)=>{
+    // find the insect by id on mongoDB
+    Insect.findById(req.params.id)
+    // then render on page
+    .then((insects)=> {
+        res.render('insects/show.ejs', {insects})
     })
 })
 
